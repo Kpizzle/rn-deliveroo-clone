bec93ca15ec599029ef822868a76307c9841af23
Kyle Potter <kp1289@gmail.com>
2022-07-10 00:59:22 +0100

    home screen updates

---
+++ screens/HomeScreen.js
@@ -0,0 +1,43 @@
+import { View, Text, SafeAreaView, Image } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+} from 'react-native-heroicons/outline';
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <Text className='text-red-500'>
+        {/* header */}
+        <View className='flex-row pb-3, items-center mx-4 space-x-2'>
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru',
+            }}
+            className='h7 w-7 bg-gray-300 p-4 rounded-full'
+          />
+          <View>
+            <Text className='font-bold text-gray-400 text-xs'>
+              Deliver Now!
+            </Text>
+            <Text className='font-bold'>Current Location</Text>
+          </View>
+        </View>
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
